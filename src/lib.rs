29ca8af1e2db294b9f266ec43a3989c36f2e23b4
hky1999 <976929993@qq.com>
2025-06-10 16:10:44 +0800

    [feat] add ivc hvcs

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,28 @@ numeric_enum! {
         HyperVisorPrepareDisable = 1,
         /// Only for debugging purposes.
         HyperVisorDebug = 2,
-
+        /// Request to setup a IVC shared memory channel as a publisher.
+        /// Arguments:
+        /// - `key`: The key of the IVC channel.
+        /// - `shm_base_gpa_ptr`: The pointer to the base address of the shared memory region in guest physical address.
+        /// - `shm_size_ptr`: The pointer to the size of the shared memory region in guest physical address.
+        HIVCPublishChannel = 3,
+        /// Request to subscriber a IVC shared memory channel.
+        /// Arguments:
+        /// - `publisher_vm_id`: The ID of the publisher VM.
+        /// - `key`: The key of the IVC channel.
+        /// - `shm_base_gpa_ptr`: The pointer to the base address of the shared memory region in guest physical address.
+        /// - `shm_size_ptr`: The pointer to the size of the shared memory region in guest physical address.
+        HIVCSubscribChannel = 4,
+        /// Request to un-register a IVC shared memory channel as a publisher.
+        /// Arguments:
+        /// - `key`: The key of the IVC channel.
+        HIVCUnPublishChannel = 5,
+        /// Request to un-subscribe a IVC shared memory channel.
+        /// Arguments:
+        /// - `publisher_vm_id`: The ID of the publisher VM.
+        /// - `key`: The key of the IVC channel.
+        HIVCUnSubscribChannel = 6,
         /// Only for debugging purposes.
         HDebug = HYPER_CALL_CODE_PRIVILEGED_MASK | 0,
         /// Create a new instance, pass the raw binary/executable file by shared pages.
@@ -53,6 +74,18 @@ impl core::fmt::Debug for HyperCallCode {
             HyperCallCode::HMMAP => write!(f, "HMMAP {:#x}", *self as u32),
             HyperCallCode::HClone => write!(f, "HClone {:#x}", *self as u32),
             HyperCallCode::HInitShim => write!(f, "HInitShim {:#x}", *self as u32),
+            HyperCallCode::HIVCPublishChannel => {
+                write!(f, "HIVCPublishChannel {:#x}", *self as u32)
+            }
+            HyperCallCode::HIVCSubscribChannel => {
+                write!(f, "HIVCSubscribChannel {:#x}", *self as u32)
+            }
+            HyperCallCode::HIVCUnPublishChannel => {
+                write!(f, "HIVCUnPublishChannel {:#x}", *self as u32)
+            }
+            HyperCallCode::HIVCUnSubscribChannel => {
+                write!(f, "HIVCUnSubscribChannel {:#x}", *self as u32)
+            }
         }?;
         write!(f, ")")
     }
